ff4ee1eaefd2b35672323ec1ea1622bc4c2d537b
Andershgras <andershgras@gmail.com>
2025-04-01 12:59:18 +0200

    mockdata og models

---
+++ FINALBOOKINGSYSTEM/MockData/MockItems.cs
@@ -0,0 +1,18 @@
+﻿using System.Reflection;
+
+namespace Bookingsystem.MockData
+{
+    public class MockItems
+    {
+        private static List<Models.Item> Items { get; set; } = new List<Models.Item>()
+        {
+            new Models.Item(1, "Lokale1", false),
+            new Models.Item(2, "Lokale2", false),
+            new Models.Item(3, "Lokale3", false)
+        };
+        public static List<Models.Item> GetMockItems()
+        {
+            return Items;
+        }
+    }
+}
---
+++ FINALBOOKINGSYSTEM/Models/Item.cs
@@ -0,0 +1,22 @@
+﻿namespace Bookingsystem.Models
+{
+    public class Item
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public bool IsBooked { get; set; }
+
+        public Item(int id, string name, bool isBooked)
+        {
+            this.Id = id;
+            this.Name = name;
+            this.IsBooked = isBooked;
+        }
+        public Item()
+        {
+            this.Id = 0;
+            this.Name = "Lokale0";
+            this.IsBooked = false;
+        }
+    }
+}
