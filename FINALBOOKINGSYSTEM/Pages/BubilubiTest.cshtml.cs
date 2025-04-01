1012fee5d201c1524f9b42249271ab7d1676224f
Jonaslk727 <joloknu@gmail.com>
2025-04-01 12:56:06 +0200

    holie

---
+++ FINALBOOKINGSYSTEM/Pages/BubilubiTest.cshtml.cs
@@ -0,0 +1,12 @@
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace FINALBOOKINGSYSTEM.Pages
+{
+    public class BubilubiTestModel : PageModel
+    {
+        public void OnGet()
+        {
+        }
+    }
+}
