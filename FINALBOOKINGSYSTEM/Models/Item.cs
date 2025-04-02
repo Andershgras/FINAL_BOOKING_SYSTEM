1386d9ea448a02db1d1d49608eb2dda325bf7d0a
bilalsarwar2907 <bilalsarwar2907@gmail.com>
2025-04-02 11:01:00 +0200

    t2

--- FINALBOOKINGSYSTEM/Models/Item.cs
+++ FINALBOOKINGSYSTEM/Models/Item.cs
@@ -3,33 +3,39 @@
 namespace FINALBOOKINGSYSTEM.Models
 {
     public class Item
-    {
-        [Display(Name = "Id")]
-        [Required(ErrorMessage = "Id is required")]
-        //[Range(typeof(int), "1", "100", ErrorMessage = "Id skal være mellem {0} og {100}")]
-        public int? Id { get; set; }
 
-        [Display(Name = "Name")]
-        [Required(ErrorMessage = "Name is required")]
-        [StringLength(50, MinimumLength = 3, ErrorMessage = "Name skal være mellem {2} og {1} tegn")]
+
+    {
+        [Required(ErrorMessage = "ID is required.")]
+        [Range(1, int.MaxValue, ErrorMessage = "ID must be greater than 0.")]
+        public int? Id { get; set; }  // Nullable to allow custom error messages
+
+        [Required(ErrorMessage = "Name is required.")]
+        [Display(Name = "Item Name")]  // Custom label for UI
         public string Name { get; set; }
 
-        //[Display(Name = "IsBooked")]
-        //[Required(ErrorMessage = "Lokale is booked")]
-        //[Range(typeof(bool), "false", "true", ErrorMessage = "Lokale is booked")]
-        //public bool IsBooked { get; set; }
+        [Required(ErrorMessage = "Booking status is required.")]
+        [Display(Name = "Is Booked?")]
+        public bool? IsBooked { get; set; }  // Nullable to allow validation
+    
 
-        public Item(int id, string name)
+
+    
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public bool IsBooked { get; set; }
+
+        public Item(int id, string name, bool isBooked)
         {
             this.Id = id;
             this.Name = name;
-            //this.IsBooked = isBooked;
+            this.IsBooked = isBooked;
         }
         public Item()
         {
             this.Id = 0;
             this.Name = "Lokale0";
-            //this.IsBooked = false;
+            this.IsBooked = false;
         }
     }
 }
